18dd36294b8b07099b550cea7ddf8c6ff23510c7
Mya Walker <86728946+MW-2005@users.noreply.github.com>
2021-07-01 17:32:41 +0000

    update

--- primary.py
+++ primary.py
@@ -1,5 +1,5 @@
-#author: <author here>
-# date: <date here>
+#author: <Mya Walker>
+# date: <7/1/21>
 
 # --------------- Section 1 --------------- #
 # ---------- Integers and Floats ---------- #
@@ -12,7 +12,11 @@
 #       2 - create a print statement that prints the sum of three numbers
 #       3 - create a print statement the prints the sum of two negative numbers
 
-
+print()
+print(100+0)
+print(90+9+1)
+print(-50+-50)
+print()
 
 # subtraction
 #   instructions
@@ -20,7 +24,11 @@
 #       2 - create a print statement that prints the difference of three numbers
 #       3 - create a print statement the prints the difference of two negative numbers
 
-
+print()
+print(101-1)
+print(102-1-1)
+print(-200--100)
+print()
 
 # multiplication and true division
 #   instructions
@@ -28,24 +36,35 @@
 #       2 - create a print statement that prints the dividend of two numbers
 #       3 - create a print statement that evaluates an operation using both multiplication and division
 
-
+print()
+print(2*50)
+print(500/5)
+print(1000*2/20)
+print()
 
 # floor division
 #   instructions
 #       1 - using floor division, print the dividend of two numbers.
 
-
+print()
+print(500//5)
+print()
 
 # exponentiation
 #   instructions
 #       1 - using exponentiation, print the power of two numbers
 
-
+print()
+print(100**1)
+print()
 
 # modulus
 #   instructions
 #       1 - using modulus, print the remainder of two numbers
 
+print()
+print(100%900)
+print()
 
 # --------------- Section 2 --------------- #
 # ---------- String Concatenation --------- #
@@ -56,14 +75,31 @@
 #       2 - print the concatenation of five animals you like
 #       3 - print the concatenation of each word in a phrase
 
-
+print()
+print('Mya'+'Walker')
+print('Dog'+'Cat'+'Bird'+'Dear'+'Fox')
+print('An'+'Apple'+'A'+'Day')
+print()
 
 # duplication
 #   instructions
 #       1 - print the duplpication of your first 5 times
 #       2 - print the duplication of a song you like 10 times
-
+print()
+print()
+print('Mya '*5)
+print()
+print('RadioGaga-Queen '*10)
+print()
 
 # concatenation and duplpication
 #   instructions
 #       1 - print the concatenation of two strings duplicated 3 times each
+
+print()
+print()
+print(('Dr.'+'Dre ')*3)
+print()
+
+
+
